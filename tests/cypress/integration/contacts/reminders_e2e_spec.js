7e6ba7782d2b2a63a219cb25585d702d8194a2ea
SebastianDiazT <sdiazt@unsa.edu.pe>
2026-07-07 23:03:08 -0500

    test(e2e): agregar caso de creación de recordatorio (RF-004.1)
    Refs #38

--- tests/cypress/integration/contacts/reminders_e2e_spec.js
+++ tests/cypress/integration/contacts/reminders_e2e_spec.js
@@ -6,10 +6,8 @@ describe("Reminder management (E2E)", function () {
   it("lets you create a reminder associated to a contact (RF-004.1)", function () {
     cy.createContact("Ada", "Lovelace", "Woman");
 
-    // profile_active_tab por defecto es 'notes', donde vive la sección
-    // de recordatorios (módulo activo por defecto en cuentas nuevas).
-    cy.get('a[href*="/reminders/add"]').click();
-    cy.url().should("include", "/reminders/add");
+    cy.get('a[href*="/reminders/create"]').first().click();
+    cy.url().should("include", "/reminders/create");
 
     cy.get("input[name=title]").type("Renovar pasaporte");
     cy.get("#frequency_type_once").click();
